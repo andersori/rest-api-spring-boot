499743ea26e0097a7262451a156531c8a3751016
Anderson Soriano <19699525+andersori@users.noreply.github.com>
2019-06-08 23:56:44 -0300

    Permitindo acesso ao frontend

--- src/main/java/com/algaworks/comercial/controller/OportunidadeController.java
+++ src/main/java/com/algaworks/comercial/controller/OportunidadeController.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,6 +21,7 @@ import org.springframework.web.server.ResponseStatusException;
 import com.algaworks.comercial.model.Oportunidade;
 import com.algaworks.comercial.repository.OportunidadeRepository;
 
+@CrossOrigin("http://localhost:4200")
 @RestController
 @RequestMapping("/oportunidades")
 public class OportunidadeController {
